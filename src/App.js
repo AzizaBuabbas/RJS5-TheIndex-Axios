73357f8b5ab61298ef7e276cdc30f48f28c7f7d7
Aziza Buabbas <aziza.buabbas@Azizas-MBP.hotspot>
2019-08-22 17:57:04 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
-
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
-
+import axios from "axios";
+import Loading from "./Loading";
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  async componentDidMount() {
+    try {
+      let response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      let authors = response.data;
+      this.setState({
+        authors: authors,
+        filteredAuthors: authors,
+        loading: false
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  selectAuthor = async author => {
+    this.setState({ loading: true });
+    try {
+      let response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
+      );
+      let author1 = response.data;
+      this.setState({ currentAuthor: author1, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -28,12 +55,15 @@ class App extends Component {
   };
 
   getContentView = () => {
+    if (this.state.loading) {
+      return <Loading />;
+    }
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
       return (
         <AuthorsList
-          authors={this.state.filteredAuthors}
+          authors={this.state.authors}
           selectAuthor={this.selectAuthor}
           filterAuthors={this.filterAuthors}
         />
--- src/BookRow.js
+++ src/BookRow.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function BookRow(props) {
   const book = props.book;
-  const author = props.author;
+  console.log("[BookRow.js] book: ", book);
+  //Map through book.authors (which is an array)
+  const authors = book.authors.map(author => <div>{author.name}</div>);
   return (
     <tr>
       <td>{book.title}</td>
-      <td>
-        {author.first_name} {author.last_name}
-      </td>
+      <td>{authors}</td>
       <td>
         <button className="btn" style={{ backgroundColor: book.color }} />
       </td>
